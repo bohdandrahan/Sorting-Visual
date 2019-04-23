9fd0c17ca5e839a4beb70751cf77401a8fc0b9d4
bohdandrahan <bohdan.drahan@gmail.com>
2019-04-22 23:03:38 -0400

    add quicksort

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -8,15 +8,47 @@ function setup() {
 	let canvas = createCanvas(width, height);
 
 	values = new Array(floor(width / w))
-	for (let i = 1; i < values.length; i++) {
-		values[i] = random(360)
+	for (let i = 0; i < values.length; i++) {
+		values[i] = random(200)
 	}
 
 	setupGUI();
 }
 
-function quickSort(arr, start, end) {
-	if (start >= end)
+async function quickSort(arr, start, end) {
+	if (start >= end) {
+		return
+	}
+	let index = await partition(arr, start, end);
+	await Promise.all([
+		quickSort(arr, start, index - 1),
+		quickSort(arr, index + 1, end)
+	]);
+
+}
+
+async function partition(arr, start, end) {
+	let pivotIndex = start;
+	let pivotValue = arr[end];
+	for (let i = start; i < end; i++) {
+		if (arr[i] < pivotValue) {
+			await swap(arr, i, pivotIndex)
+			pivotIndex++;
+		}
+	}
+	await swap(arr, pivotIndex, end);
+	return pivotIndex;
+}
+
+async function swap(arr, a, b) {
+	await sleep(100)
+	let temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function draw() {
@@ -25,8 +57,7 @@ function draw() {
 		stroke(0)
 		colorMode(HSB)
 		fill(values[i], 100, 100);
-		rect(i * w + w / 2, 40, w, 100)
-
+		rect(i * w, 100, w, 100)
 	}
 }
 
@@ -34,8 +65,17 @@ function setupGUI() {
 	resetButton = createButton('reset');
 	resetButton.mousePressed(reset_values);
 	resetButton.position(10, 20)
+
+	sortButton = createButton('sort');
+	sortButton.mousePressed(sortPressed);
+	sortButton.position(10, 40)
+}
+
+function sortPressed() {
+	quickSort(values, 0, values.length - 1)
 }
 
 function reset_values() {
+	console.log('reset')
 
 }
